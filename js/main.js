fa28422eaadaa6a55ab0b1c0ed31daa0b7616851
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-04-07 19:59:30 +0800

    Allow more configuration

--- js/main.js
+++ js/main.js
@@ -10,15 +10,24 @@ async function main() {
   sendSerial("G60");
   sendSerial("G91");
 
-  let x_inc = INCREMENT;
-  for (let y = 0; y < Y_LIMIT; y++) {
-    for (let x = 0; x < X_LIMIT; x++) {
-      sendSerial(`G0 X${x_inc}`);
-      await delay(500);
-      takePhoto().then((imageStr) => saveImage(imageStr, "image_A.png"));
+  const increment = document.querySelector("#movement-increment").value;
+  let x_inc = increment;
+  const x_limit = document.querySelector("#x-travel").value;
+  const y_limit = document.querySelector("#y-travel").value;
+  const feedrate = document.querySelector("#feedrate").value;
+  const delay_ms = document.querySelector("#delay-ms").value;
+  const image_name = document.querySelector("#image-prefix").value;
+
+  for (let y = 0; y < Math.floor(y_limit / increment); y++) {
+    for (let x = 0; x < Math.floor(x_limit / increment); x++) {
+      sendSerial(`G0 X${x_inc} F${feedrate}`);
+      await delay(delay_ms);
+      takePhoto().then((imageStr) => saveImage(imageStr, image_name));
     }
-    sendSerial(`G0 Y${INCREMENT}`);
+    sendSerial(`G0 Y${INCREMENT} F${feedrate}`);
     x_inc = -x_inc;
-    await delay(500);
+    await delay(delay_ms);
   }
+
+  sendSerial("G61");
 }
